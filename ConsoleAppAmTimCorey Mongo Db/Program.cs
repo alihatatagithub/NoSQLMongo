5cc7460820cb1270a75b365c650f66ad88d79d85
ali <alihatata055@gmail.com>
2021-10-18 18:40:02 +0200

    Add project files.

---
+++ ConsoleAppAmTimCorey Mongo Db/Program.cs
@@ -0,0 +1,94 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppAmTimCorey_Mongo_Db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBookLast");
+            //db.InsertRecord("Users", new PersonModel { FirstName = "Amr",LastName = "Mousa"});
+            var recs = db.LoadRecords<PersonModel>("User");
+            foreach (var rec in recs)
+            {
+                Console.WriteLine($" {rec.FirstName}");
+            }
+          var onerec = db.LoadRecordById<PersonModel>("Users", new Guid("pZUJXlBAvkianYsVLqcebA=="));
+            onerec.BirthDate = new DateTime(1998, 9, 17, 0, 0, 0, DateTimeKind.Utc);
+            db.UpsertRecord("Users", onerec.Id, onerec);
+            db.DeleteRecord<PersonModel>("Users", onerec.Id);
+
+        }
+
+    }
+
+    public class NameModel
+    {
+        [BsonId] //_id
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class PersonModel
+    {
+        [BsonId] //_id
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [BsonElement("DOB")]
+        public DateTime BirthDate { get; set; }
+
+    }
+
+    public class MongoCRUD
+    {
+
+        IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+            
+
+        }
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collections = db.GetCollection<T>(table);
+            collections.InsertOne(record);
+
+        }
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            return collection.Find(filter).First();
+
+        }
+        public void UpsertRecord<T>(string table,Guid id,T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            var result = collection.ReplaceOne(new BsonDocument("_id", id), record, new ReplaceOptions { IsUpsert = true });
+        }
+
+        public void DeleteRecord<T> (string table,Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+
+        }
+
+
+    }
+}
---
+++ MongoDb/Program.cs
@@ -0,0 +1,76 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            //PersonModel person = new PersonModel
+            //{
+            //    FirstName = "Joe ",
+            //    LastName = "Smith",
+            //    PrimaryAddress = new AddressModel
+
+            //    {
+            //        StreetAddress = "101 oak street",
+            //        City = "Scranton",
+            //        State="SA",
+            //        ZipCode="18512"
+
+            //    }
+            //};
+            //db.InsertRecord("Users",person);
+            var recs = db.LoadRecord<PersonModel>("Users");
+            foreach (var rec in recs)
+            {
+                Console.WriteLine($"{rec.Id}:{rec.FirstName}:{rec.LastName}");
+            }
+
+            Console.ReadLine(); ;
+        }
+
+    }
+    public class PersonModel
+    {
+        [BsonId] //_id
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+    }
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+    public class MongoCRUD
+    {
+       private IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+
+        }
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+        public List<T> LoadRecord<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+    }
+
+}
